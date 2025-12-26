362675b7b8b486f814f1c7c4823dde5c6addb348
Oskar Nizioł <oskarniziol@gmail.com>
2025-12-27 00:06:58 +0100

    feat(wifi): set hostname

--- components/wifi/wifi.c
+++ components/wifi/wifi.c
@@ -63,6 +63,8 @@ esp_err_t wifi_init(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     s_netif_sta = esp_netif_create_default_wifi_sta();
 
+    ESP_ERROR_CHECK(esp_netif_set_hostname(s_netif_sta, CONFIG_WIFI_HOSTNAME));
+
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
